f710725848bcf7f8884f696252a024427b59d22b
Jesus Nery <jrnery18@hotmail.com>
2026-05-19 19:37:54 -0300

    feat: adiciona script de criacao da tabela Contrato

--- database/create_table_Contrato.sql
+++ database/create_table_Contrato.sql
@@ -2,7 +2,7 @@ CREATE TABLE Contrato (
     Numero_Contrato SERIAL PRIMARY KEY,
     client_id INT REFERENCES clients(id) ON DELETE CASCADE,
     User_id INT REFERENCES users(id) ON DELETE CASCADE,
-    valor_total NUMERIC(10, 2) NOT NULL,
-    valor_hora NUMERIC(10, 2),
+    valor_total NUMERIC(12, 2) NOT NULL,
+    valor_hora NUMERIC(12, 2),
     prazo_estimado INTERVAL
 );
